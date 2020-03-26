6a0ebbfdb15bebf171d9fa1f41bbdd99c76354f1
Lidiane c Silva <loveliedi@gmail.com>
2020-03-26 10:56:01 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+
+
+ // area de atuação maior
+
+//JSON Javscript Object Notation =  notacao do objeto literal
+// ` crase template string que vc pode adicionar strings sem quebrar
+// Eventos no Javscript
+// ${} acrescentar um comando js.
+
+
+//SPA SINGLE PAGE APLICATION - APLICAÇÃO DE UMA ÚNICA PÁGINA...TEND~ENCIA FAZ TUDO EM UMA PÁGINA SÓ
+
+//function
+
+let userController = new UserController('form-user-create', 'table-user');
+
